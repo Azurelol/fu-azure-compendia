ba7c98f2e04bf3bd62eb915e701fa9922b165341
Christian Sagel <ckpsm@live.com>
2025-02-17 21:07:20 +0100

    Redo the implementation to handle traits

--- module/events.mjs
+++ module/events.mjs
@@ -4,102 +4,200 @@ import { AzureCompendiaSettings } from "./settings.mjs";
 const assetsDirectory = "modules/fu-azure-compendia/assets";
 const soundsDirectory = assetsDirectory + "/sounds";
 
+// Uses: https://fantasycomputer.works/FoundryVTT-Sequencer/#/
 /**
- * @typedef Preset
- * @property {String} animation
+ * @property {String[]} animations
  * @property {String} sound
  */
+class Preset {
+    constructor(...animations) {
+        this.animations = animations;
+    }
 
+    withSound(sound) {
+        this.sound = sound;
+        return this;
+    }
+
+    get animation() {
+        const length = this.animations.length;
+        if (length > 1) {
+            const randomIndex = Math.floor(Math.random() * length);
+            return this.animations[randomIndex];
+        }
+        return this.animations[0];
+    }
+}
+
+/**
+ * @type {Object<String, Preset>}
+ */
 const presets = Object.freeze({
-    ice: {
-        animation: "jb2a.impact_themed.ice_shard.blue",
-        sound: ""
-    },
-    fire: {
-        animation: "jb2a.cast_generic.fire.01.orange.0",
-        sound: ""
-    },
-    bolt: {
-        animation: "jb2a.thunderwave.center.blue",
-        sound: ""
-    },
-    earth: {
-        animation: "jb2a.cast_generic.earth.01.browngreen.0",
-        sound: ""
-    },
-    dark: {
-        animation: "jb2a.healing_generic.200px.purple",
-        sound: ""
-    },
-    light: {
-        animation: "jb2a.twinkling_stars.points04.white",
-        sound: ""
-    },
-    poison: {
-        animation: "jb2a.energy_strands.in.green.01.0",
-        sound: ""
-    },
-    air: {
-        animation: "jb2a.energy_strands.overlay.blue.01",
-        sound: ""
-    },
-    physical: {
-        animation: "jb2a.impact.007.yellow",
-        sound: ""
-    },
-    untyped: {
-        animation: "jb2a.cast_generic.fire.01.orange.0",
-        sound: ""
-    },
-    none: {
-        animation: "",
-        sound: ""
-    },
-    hp: {
-        animation: "jb2a.healing_generic.200px.green",
-        sound: ""
-    },
-    mp: {
-        animation: "jb2a.healing_generic.200px.blue",
-        sound: ""
-    },
+    // Damage Types
+    ice: new Preset("jb2a.impact_themed.ice_shard.blue"),
+    fire: new Preset("jb2a.cast_generic.fire.01.orange.0"),
+    bolt: new Preset("jb2a.thunderwave.center.blue"),
+    earth: new Preset("jb2a.cast_generic.earth.01.browngreen.0"),
+    dark: new Preset("jb2a.healing_generic.200px.purple"),
+    light: new Preset("jb2a.twinkling_stars.points04.white"),
+    poison: new Preset("jb2a.energy_strands.in.green.01.0"),
+    air: new Preset("jb2a.energy_strands.overlay.blue.01"),
+    physical: new Preset("jb2a.impact.007.yellow"),
+    untyped: new Preset("jb2a.cast_generic.fire.01.orange.0"),
+    // Resources    
+    hp: new Preset("jb2a.healing_generic.200px.green"),
+    mp: new Preset("jb2a.healing_generic.200px.blue"),
+    // Weapons
+    bow: new Preset("jb2a.arrow.physical.blue"),
+    sword: new Preset("jb2a.sword.melee.01.white.0"),
+    dagger: new Preset("jb2a.dagger.melee.02.white"),
+    spear: new Preset("jb2a.spear.melee.01.white.0"),
+    heavy: new Preset("jb2a.melee_attack.02.battleaxe"),
+    brawling: new Preset("jb2a.unarmed_strike.physical"),
+    thrown: new Preset("jb2a.dagger.throw"),
+    firearm: new Preset("jb2a.bullet")
 });
 
-async function playPreset(name, token) {
-    const preset = presets[name];
-    if (preset) {
-        if (preset.sound) {
-            playSoundEffect(preset.sound);
-        }
-        if (preset.animation) {
-            playAnimation(preset.animation, token);
+/**
+ * @description Dispatched when an actor suffers damage
+ * @typedef DamageEvent
+ * @property {FU.damageTypes} type
+ * @property {Number} amount
+ * @property {FUActor} actor
+ * @property {Token} token
+ * @property {FUActor} sourceActor
+ * @property {Token} sourceToken
+ * @property {Set<String>} traits
+ */
+
+/**
+ * @typedef {"arcane", "bow", "brawling", "dagger", "firearm", "flail", "heavy", "spear", "sword", "thrown", "custom"} WeaponCategory
+ */
+
+///////////////////////////////
+const supportedWeapons = new Set([
+    "arcane",
+    "bow",
+    "brawling",
+    "dagger",
+    "firearm",
+    "flail",
+    "heavy",
+    "spear",
+    "sword",
+    "thrown",
+    "custom"
+]);
+
+function getWeapon(traits) {
+    const chosenWeapons = [...supportedWeapons].filter(item => traits.has(item));
+    if (chosenWeapons.length == 1) {
+        return chosenWeapons[0]
+    }
+    return null;
+}
+///////////////////////////////
+
+/**
+ * @param {DamageEvent} event 
+ */
+async function playDamagePreset(event) {
+
+    if (event.traits.has("melee")) {
+        const weapon = getWeapon(event.traits);
+        if (weapon) {
+            await playMeleeAnimation(presets[weapon], event.sourceToken, event.token);
         }
     }
+    else if (event.traits.has("ranged")) {
+        const weapon = getWeapon(event.traits);
+        if (weapon) {
+            await playRangedAnimation(presets[weapon], event.sourceToken, event.token);
+        }
+    }
+
+    // Damage on token based on type
+    await playAnimationOnToken(presets[event.type], event.token)
 }
 
 async function playSoundEffect(name) {
+    if (!name) {
+        return;
+    }
     const path = `${soundsDirectory}/${name}.ogg`
     console.debug(`Playing sound ${path}`);
-    foundry.audio.AudioHelper.play({ src: path, volume: 0.5 }, true);
-}
+    // foundry.audio.AudioHelper.play({ src: path, volume: 0.5 }, true);
 
-// Uses: https://fantasycomputer.works/FoundryVTT-Sequencer/#/
-async function playAnimation(path, token) {
-    if (!game.modules.get("sequencer")?.active) {
-        console.debug("Sequencer not installed and active!");
-    }
-
-    if (!game.modules.get("JB2A_DnD5e")?.active) {
-        console.debug("JB2A module not installed and active!");
-    }
 
     new Sequence()
+        .sound()
+        .file(path)  // Specify your sound file
+        .volume(0.5)  // Adjust volume (optional)
+        .delay(100)   // Optional delay before sound plays (in milliseconds)
+        .play();
+}
+
+
+/**
+ * @param {Preset} preset 
+ */
+async function playAnimationOnToken(preset, token) {
+
+    playSoundEffect(preset.sound);
+    const path = preset.animation;
+    return new Sequence()
         .effect()
         .file(path)
         .atLocation(token)
         .scaleToObject(1.5, {
-            considerTokenScale : true
+            considerTokenScale: true
         })
+        .play();
+}
+
+/**
+ * @param {Preset} preset 
+ */
+async function playMeleeAnimation(preset, sourceToken, targetToken) {
+    if (!sourceToken || !targetToken) {
+        return;
+    }
+
+    if (!preset) {
+        Azurecompendia.log("No preset was given");
+        return;
+    }
+
+    playSoundEffect(preset.sound);
+    const path = preset.animation;
+    return new Sequence()
+        .effect()
+        .file(path)
+        .atLocation(sourceToken)
+        .rotateTowards(targetToken)
+        .play();
+}
+
+/**
+ * @param {Preset} preset 
+ */
+async function playRangedAnimation(preset, sourceToken, targetToken) {
+    if (!sourceToken || !targetToken) {
+        return;
+    }
+
+    if (!preset) {
+        Azurecompendia.log("No preset was given");
+        return;
+    }
+
+    playSoundEffect(preset.sound);
+    const path = preset.animation;
+    return new Sequence()
+        .effect()
+        .file(path)
+        .atLocation(sourceToken)
+        .stretchTo(targetToken)
         .play();
 }
 
@@ -111,14 +209,24 @@ function subscribe() {
         return;
     }
 
+    if (!game.modules.get("sequencer")?.active) {
+        console.debug("Sequencer not installed and active!");
+        return;
+    }
+
+    if (!game.modules.get("JB2A_DnD5e")?.active) {
+        console.debug("JB2A module not installed and active!");
+        return;
+    }
+
     Hooks.on('projectfu.events.damage', async event => {
-        Azurecompendia.log(`Playing preset for damage event: ${event.type} on token: ${event.token.name}`);
-        playPreset(event.type, event.token);
+        Azurecompendia.log(`Playing preset for damage event: ${event.type} on token: ${event.token.name} with traits: ${event.traits}`);
+        playDamagePreset(event);
     });
 
     Hooks.on('projectfu.events.gain', async event => {
         Azurecompendia.log(`Playing preset for gain event: ${event.resource} on token: ${event.token.name}`);
-        playPreset(event.resource, event.token);
+        playAnimationOnToken(presets[event.resource], event.token);
     });
 
     Hooks.on('projectfu.events.loss', async event => {
